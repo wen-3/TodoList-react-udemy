75029e1e140572caf57f6ac155525cd0154c2432
wen-3 <sd63324@gmail.com>
2024-08-12 23:14:39 +0800

    Create-Editor-component

---
+++ src/Todos/Editor.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+
+export const Editor: FC = () => {
+    return (
+        <div className="box">
+            <div className="field">
+                <div className="control">
+                    <input type="text" className="input" placeholder="title" />
+                </div>
+            </div>
+            <div className="columns is-vcentered">
+                <div className="column">
+                    <div className="field">
+                        <div className="control">
+                            <label className="radio"><input type="radio" />High</label>
+                            <label className="radio"><input type="radio" />Medium</label>
+                            <label className="radio"><input type="radio" />Low</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="column has-text-right">
+                    <div className="field">
+                        <div className="control">
+                            <div className="select">
+                                <select>
+                                    <option>assigned to</option>
+                                    <option>alex</option>
+                                    <option>bob</option>
+                                    <option>chris</option>
+                                    <option>david</option>
+                                    <option>ed</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="field">
+                <div className="control">
+                    <textarea className="textarea" placeholder="content" />
+                </div>
+            </div>
+
+            <div className="columns">
+                <div className="column">
+                    <div className="field">
+                        <div className="control">
+                            <label className="checkbox">
+                                <input type="checkbox" />Resolved
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="field is-grouped is-grouped-right">
+                        <div className="control">
+                            <div className="buttons has-addons">
+                                <button className="button is-link">Save</button>
+                                <button className="button is-link is-light">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
