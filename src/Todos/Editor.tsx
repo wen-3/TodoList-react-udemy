b89d7f5db4324e6f8d19601ec73587dd6177a566
wen-3 <sd63324@gmail.com>
2024-08-12 23:34:12 +0800

    Radio

--- src/Todos/Editor.tsx
+++ src/Todos/Editor.tsx
@@ -1,23 +1,26 @@
 import { ChangeEventHandler, FC, useState } from "react";
+import { Priority } from "./TodoItem";
 
 export const Editor: FC = () => {
     const [title, setTitle] = useState<string>('');
+    const [priority, setPriority] = useState<Priority>(Priority.LOW);
     const handleTitleChange: ChangeEventHandler<HTMLInputElement> = e => setTitle(e.target.value);
+    const handlePriorityChange: ChangeEventHandler<HTMLInputElement> = e => setPriority(parseInt(e.target.value));
 
     return (
         <div className="box">
             <div className="field">
                 <div className="control">
-                <input type="text" className="input" placeholder="title" value={title} onChange={handleTitleChange} />
+                    <input type="text" className="input" placeholder="title" value={title} onChange={handleTitleChange} />
                 </div>
             </div>
             <div className="columns is-vcentered">
                 <div className="column">
                     <div className="field">
                         <div className="control">
-                            <label className="radio"><input type="radio" />High</label>
-                            <label className="radio"><input type="radio" />Medium</label>
-                            <label className="radio"><input type="radio" />Low</label>
+                            <label className="radio"><input type="radio" checked={priority === 0} value={Priority.HIGH} onChange={handlePriorityChange} />High</label>
+                            <label className="radio"><input type="radio" checked={priority === 1} value={Priority.MEDIUM} onChange={handlePriorityChange} />Medium</label>
+                            <label className="radio"><input type="radio" checked={priority === 2} value={Priority.LOW} onChange={handlePriorityChange} />Low</label>
                         </div>
                     </div>
                 </div>
